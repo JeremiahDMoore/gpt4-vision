0859c6d11cc7c8b75efe4ad5d563fca0fddd03d2
Jeremiah <23569464+JeremiahDMoore@users.noreply.github.com>
2023-12-31 12:37:19 -0700

    added recipe generator functionality

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
 
-const foodPrompt = "PERSONA = (You are an expert chef, foodie and nutritionist. You are able to generate gourmet recipes for all types of cuisine. You understand the nutritional value of all foods and drinks. Your tone is jovial, very humorous and funny. You make your responses as concise as possible. You use Bullet Points when listing Nutritional Value and Ingredients at all times.) ; FOOD_DETAIL = (any cuisine, dish, meal, cocktail, beverage, food items, drink items, meals, food ingredients, food packages and labels, drink packages and labels; anything pertaining to food or food culture.) ; INSTRUCTIONS: You will adhere to your PERSONA at all times. You will examine the photo and determine if it contains a FOOD_DETAIL ; IF: FOOD_DETAIL = false, return ( name what is in the photo, You don't really want to eat this, do you?);  ELSE IF: FOOD_DETAIL = true, return ({ Name: a short description of the photo, Nutritional Value: the caloric and nutritional content of this FOOD_DETAIL; Recipe: a recipe with ingredient list to recreate this FOOD_DETAIL })"
+const foodPrompt = "PERSONA = (You are an expert chef, foodie and nutritionist. You are able to generate gourmet recipes for all types of cuisine. You understand the nutritional value of all foods and drinks. Your tone is jovial, very humorous and funny. You make your responses as concise as possible. You use Bullet Points when listing Nutritional Value and Ingredients at all times.) ; FOOD_DETAIL = (any cuisine, dish, meal, cocktail, beverage, food items, drink items, meals, food ingredients, food packages and labels, drink packages and labels; anything pertaining to food or food culture.) ; INSTRUCTIONS: You will adhere to your PERSONA at all times. You will examine the photo and determine if it contains a FOOD_DETAIL ; IF: FOOD_DETAIL = false, return ( {Name: a short description of the photo}, You don't really want to eat this, do you?);  ELSE IF: FOOD_DETAIL = true, return ({ Name: a short description of the photo, Nutritional Value: the caloric and nutritional content of this FOOD_DETAIL; Recipe: a recipe with ingredient list to recreate this FOOD_DETAIL })"
+
+function generatePrompt(dishName, diet, otherConsiderations) {
+  return `you will generate a recipe based on ${dishName} and ${otherConsiderations} that is ${diet}`
+}
 
 app.post('/process-image', async (req, res) => {
   try {
@@ -17,12 +21,50 @@ app.post('/process-image', async (req, res) => {
     res.status(500).send({ error: error.message });
   }
 });
+// New endpoint for recipe generation
+app.post('/generate-recipe', async (req, res) => {
+  try {
+    const { dishName, diet, otherConsiderations } = req.body;
+
+    // Call an external server or implement your recipe generation logic here
+    // For demonstration, I'm just logging the received data
+    console.log('Received recipe request:', { dishName, diet, otherConsiderations });
+    
+    const recipe = await axios.post('https://api.openai.com/v1/chat/completions', {
+      model: "gpt-3.5-turbo-16k-0613",  // Use the correct model identifier here
+      temperature: 0.8,
+      top_p: 1,
+      messages: 
+          { role: "system", content: generatePrompt(dishName, diet, otherConsiderations) },
+ 
+      headers: {
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+      }
+  });
+ 
+       // Check if the API response is successful and contains data
+       if (recipe && recipe.data && recipe.data.choices && recipe.data.choices.length > 0) {
+        // Extracting the text from the first choice (most relevant response)
+        const recipeText = recipe.data.choices[0].text;
+  
+        // Send the actual recipe text as response
+        res.json({ success: true, recipe: recipeText });
+      } else {
+        // In case the API response doesn't have the expected format or is empty
+        res.status(500).json({ success: false, message: 'Failed to generate recipe' });
+      }
+    } catch (error) {
+      // Error handling remains the same
+      res.status(500).send({ error: error.message });
+    }
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
 
 dotenv.config();
 
@@ -44,7 +86,7 @@ async function askAboutImages(base64Image, prompt) {
         ],
       },
     ],
-    max_tokens: 400,
+    max_tokens: 1000,
   });
 
   console.log('RESPONSE HERE', response.choices[0].message.content);
