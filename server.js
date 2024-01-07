77ed67421a9cf12ccfbf16c6c4008075bbd0c32c
Jeremiah <23569464+JeremiahDMoore@users.noreply.github.com>
2024-01-06 18:45:51 -0700

    prompt tweak, dalle-3 test

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const Jimp = require('jimp');
 
 const app = express();
 app.use(bodyParser.json({ limit: '50mb' }));
 
 // PHOTO: prompt for extracting data from photo
 // TODO: lean the prompt to cut API costs
-const foodPrompt = "PERSONA = (You are an expert chef, foodie and nutritionist. You are able to generate gourmet recipes for all types of cuisine. You understand the nutritional value of all foods and drinks. Your tone is jovial, very humorous and funny. You make your responses as concise as possible. You use Bullet Points when listing Nutritional Value and Ingredients at all times.) ; FOOD_DETAIL = (any cuisine, dish, meal, cocktail, beverage, food items, drink items, meals, food ingredients, food packages and labels, drink packages and labels; anything pertaining to food or food culture.) ; INSTRUCTIONS: You will adhere to your PERSONA at all times. You will examine the photo and determine if it contains a FOOD_DETAIL ; IF: FOOD_DETAIL = false, return ( a short description of the photo or name of object, You don't really want to eat this, do you?);  ELSE IF: FOOD_DETAIL = true, return ({ a short description of the FOOD_DETAIL, Nutritional Value: the caloric and nutritional content of this FOOD_DETAIL; Recipe: a recipe with ingredient list to recreate this FOOD_DETAIL })"
-
+const foodPrompt = "PERSONA = (You are an expert chef, foodie and nutritionist. You are able to generate gourmet recipes for all types of cuisine. You understand the nutritional value of all foods and drinks. Your tone is jovial, very humorous and funny. Your goal is to create delicious recipes for all humans to enjoy.) ; FOOD_DETAIL = (any cuisine, dish, meal, cocktail, beverage, food items, drink items, meals, food ingredients, food packages and labels, drink packages and labels; anything pertaining to food or food culture.) ; INSTRUCTIONS: Adhere to your PERSONA at all times. Examine the photo and determine if it contains a FOOD_DETAIL ; IF: FOOD_DETAIL = false, return ( a short description of the photo or name of object, You don't really want to eat this, do you?);  ELSE IF: FOOD_DETAIL = true, return ({ a short description of the FOOD_DETAIL, Nutritional Value: the caloric and nutritional content of this FOOD_DETAIL; Recipe: a recipe with ingredient list to recreate this FOOD_DETAIL }) Make your responses as concise as possible. Use Bullet Points when listing Nutritional Value and Ingredients at all times"
 // IMAGE: prompt for generating an image for the RECIPE
 function generateImage(dishName, diet, otherConsiderations) {
   return `A lifelike food photo of ${dishName}, ${diet}, ${otherConsiderations}, plated fancy on a chef table, blurred background of ingredients, realistic indoor lighting. (Taken on a Sony camera)[restaraunt marketing photo, realistic]`
@@ -62,41 +60,34 @@ app.post('/generate-recipe', async (req, res) => {
 
 // IMAGE: generates an image of the recipe
 app.post('/generate-image', async (req, res) => {
-    try {
-        const { dishName, diet, otherConsiderations } = req.body;
-        const prompt = generateImage(dishName, diet, otherConsiderations);
+  try {
+    const { dishName, diet, otherConsiderations } = req.body;
+    const prompt = generateImage(dishName, diet, otherConsiderations);
 
-        // Generate image using DALL-E
-        const response = await axios.post('https://api.openai.com/v1/images/generations', {
-            model: "dall-e-3",
-            prompt: prompt,
-            n: 1,
-            size: "1024x1024"
-        }, {
-            headers: {
-                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-                'Content-Type': 'application/json'
-            }
-        });
+    const response = await axios.post('https://api.openai.com/v1/images/generations', {
+      model: "dall-e-3",
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024"
+    }, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    });
 
-        // Fetch the generated image
-        const imageUrl = response.data.data[0].url;
-        const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-        const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+    const imageUrl = response.data.data[0].url;
+    res.json({ success: true, recipe: imageUrl });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
 
-        // Resize the image to 400x400 using Jimp
-        const resizedImage = await Jimp.read(imageBuffer)
-            .then(image => image.resize(400, 400))
-            .then(image => image.getBufferAsync(Jimp.MIME_PNG));
 
-        // Send the resized image directly to the client
-        res.set('Content-Type', 'image/png');
-        res.send(resizedImage);
 
-    } catch (error) {
-        console.error('Error generating image:', error);
-        res.status(500).send({ error: error.message });
-    }
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 dotenv.config();
